49e11b7d121af8c537089d1f57fbfcedddce9308
bihire <muhireboris@yahoo.fr>
2020-01-26 09:55:24 +0200

    fix tests locally

--- src/server.js
+++ src/server.js
@@ -3,14 +3,9 @@ import app from './app';
 
 const PORT = process.env.PORT || 5000;
 
-let server;
 sequelize
   .sync({
     force: false
-  })
-  .then(() => {
-    server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   });
 
-
-export default server;
+export default app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
