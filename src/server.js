2de749a080679a81221ddbad2c0d10b1419984ec
bihire <muhireboris@yahoo.fr>
2020-01-26 07:54:39 +0200

    run the serve after the creation of tables

--- src/server.js
+++ src/server.js
@@ -3,14 +3,14 @@ import app from './app';
 
 const PORT = process.env.PORT || 5000;
 
+let server;
 sequelize
   .sync({
     force: false
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   });
 
-const server = app.listen(3000);
 
 export default server;
