881b1568a8ff3895ef2277e9202e3fd70e6a4ee0
florisngenda <florisngendahayo@gmail.com>
2020-01-29 16:59:48 +0200

    Increasing the test coverage and integrating Eslint

--- src/middleware/loginValidation.js
+++ src/middleware/loginValidation.js
@@ -1,18 +1,17 @@
 import Joi from 'joi';
 
 class Validator {
-    
-    constructor() {
-        this.created = new Date().toString();
-      }
-  schemaSignIn(dataToValidate) {
+  constructor() {
+    this.created = new Date().toString();
+  }
+
+  static schemaSignIn(dataToValidate) {
     const inSchema = {
       email: Joi.string().email().required(),
       password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required().trim(),
     };
     return Joi.validate(dataToValidate, inSchema);
   }
-
 }
 
 const expValidator = new Validator();
