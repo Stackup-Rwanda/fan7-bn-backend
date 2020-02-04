37ba62d150dfdaf584b8446c6867ab88eb62adeb
Elvis-rugamba <elvisrugamba@gmail.com>
2020-02-05 00:39:44 +0200

    ft(Approve requests): Manager approves requests - generate Request model - Define association - create middleware - create route - create request repository
    [Finishes #170764590]

---
+++ src/database/migrations/20200204215449-create-request.js
@@ -0,0 +1,34 @@
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Requests', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'user_id',
+      },
+    },
+    status: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.fn('NOW')
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.fn('NOW')
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Requests')
+};
