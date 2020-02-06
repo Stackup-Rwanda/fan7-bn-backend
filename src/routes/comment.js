d9b819182892ca8de07e6ff92f3dcffde0975dc0
florisngenda <florisngendahayo@gmail.com>
2020-02-07 00:04:03 +0200

    --amend

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import Controller from '../controllers/commentController';
 import AuthMiddleware from '../middlewares/auth.middleware';
-import Access from '../middlewares/auth.middleware';
 import Validation from '../middlewares/comment.middleware';
 
 
@@ -9,6 +8,6 @@ const router = express.Router();
 
 router.post('/:id/comment', Validation.CommentValidate, AuthMiddleware.verifyToken, Controller.addComment);
 router.get('/:id/comments', AuthMiddleware.verifyToken, Controller.getCommentsByRequest);
-router.delete('/comment/:id', Access.isCommentOwner, AuthMiddleware.verifyToken, Controller.deleteComment);
+router.delete('/comment/:id', AuthMiddleware.isCommentOwner, AuthMiddleware.verifyToken, Controller.deleteComment);
 
 export default router;
